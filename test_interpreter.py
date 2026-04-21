4afe6c877f2b77d36bc399a4a4efcb9ac011ec65
Ashutosh Kaushik <ashutosh.kaushik@fiftyfivetech.io>
2026-04-21 14:41:33 +0530

    feat: sync all code to remote feature branch

---
+++ test_interpreter.py
@@ -0,0 +1,4 @@
+from src.rag.data_interpreter import DataInterpreter
+interpreter = DataInterpreter()
+res = interpreter.interpret("Total count of praises for Aman Khandelwal?")
+print("RESULT:", res)
